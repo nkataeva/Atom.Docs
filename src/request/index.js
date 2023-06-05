ec3ffa93a7a74fcc5649998e1ff2831902bce86d
Софья Воропай <sofiavoropai@MacBook-Air-Sofa.local>
2023-06-05 07:03:46 +0300

    2

--- src/request/index.js
+++ src/request/index.js
@@ -1,14 +1,45 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import './request.css'
-export function Request() {
+export function Request ({ term, data, update }) {
+
+    const dataSearch = e => {
+      const value = e.target.value.toLowerCase();
+  
+      const filter = data.filter(doc => {
+        return doc.toLowerCase().includes(value);
+      });
+  
+      update({
+        data: filter,
+        active: 0,
+        term: value
+      });
+      
+    };
     return (
-        <div class="main">
-            <h1>Заявки</h1>
+       
+        <div class="container">
+             <div class="header">
+            <img src="./logo.svg"/>
+             </div>
+        <div class="first">
+        
+            <h1 class="text">Заявки</h1>
             <div class="one">
-                    <Link to='/creature'>Создать заявку</Link> 
-                    <button>Поиск заявок</button>
-            </div>
+                <button>Создать</button>
+            <div classe="Search">
+      <span class="SearchSpan">
+       
+      </span>
+      <input class="SearchInput"
+      value={term}
+        type="text"
+        onChange={dataSearch}
+        placeholder="Поиск"
+      />
+      </div>
+    </div>
             <table class="table">
 	<thead>
 		<tr>
@@ -18,11 +49,36 @@ export function Request() {
 			<th>Документ</th>
 			
 		</tr>
-	</thead>
+    </thead>
+    <tbody>
+        
+        <tr>
+			<th>1</th>
+			<th>first app</th>
+			<th>not</th>
+			<th>file</th>
+			
+		</tr>
+        <tr>
+			<th>2</th>
+			<th>two app</th>
+			<th>active</th>
+			<th>not</th>
+			
+		</tr>
+        <tr>
+			<th>3</th>
+			<th>three app</th>
+			<th>active</th>
+			<th>file</th>
+			
+		</tr>
+        </tbody>
+	
 	
         
 </table>   
-            
+</div>
         </div>
     )
 }
